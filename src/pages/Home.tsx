06d345f09d6f667bcec00348e934170da3db0ba9
Marco Echevestre <devminotto@gmail.com>
2021-06-22 17:05:13 +0200

    Create auth page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Illustration representing questions and answers"
+        />
+        <strong>Create rooms of live Q&amp;A</strong>
+        <p>Answer questions of your followers in real time</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask logo" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Google logo" />
+            Create your room with Google
+          </button>
+          <div className="separator">Or join a room</div>
+          <form>
+            <input type="text" placeholder="Type the room's code" />
+            <Button type="submit">Join the room</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
