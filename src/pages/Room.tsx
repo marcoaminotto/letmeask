cc5fcbe688d27a098a0cde916b07e801a823675c
Marco Echevestre <devminotto@gmail.com>
2021-06-25 01:07:11 +0200

    Create Question component and implement styles

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,6 +4,7 @@ import { toast, Toaster } from 'react-hot-toast';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 
@@ -24,7 +25,7 @@ type FirebaseQuestions = Record<
   }
 >;
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -43,7 +44,7 @@ export function Room() {
   const params = useParams<RoomParams>();
   const { user } = useAuth();
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
   const roomId = params.id;
@@ -143,7 +144,17 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="questions-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
