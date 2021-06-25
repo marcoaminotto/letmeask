d879990c997701a64310892c34e924665f4b748d
Marco Echevestre <devminotto@gmail.com>
2021-06-25 13:56:45 +0200

    Implement functions and styles of isAnswered and isHilighted buttons

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -12,7 +12,7 @@ type FirebaseQuestions = Record<
     };
     content: string;
     isAnswered: boolean;
-    isHighLighted: boolean;
+    isHighlighted: boolean;
     likes: Record<
       string,
       {
@@ -30,7 +30,7 @@ type QuestionType = {
   };
   content: string;
   isAnswered: boolean;
-  isHighLighted: boolean;
+  isHighlighted: boolean;
   likeCount: number;
   likedId: string | undefined;
 };
@@ -53,7 +53,7 @@ export function useRoom(roomId: string) {
             id: key,
             content: value.content,
             author: value.author,
-            isHighLighted: value.isHighLighted,
+            isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
             likeCount: Object.values(value.likes ?? {}).length,
             likedId: Object.entries(value.likes ?? {}).find(
